70e6837b9ecf11c7b03bb3414c37cc4f2c29c6c4
Vikrant <Vikrant@SEASIAINFOTECH.COM>
2022-06-20 23:07:02 +0530

    GIT API

---
+++ filter.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+UserName = input("Enter your Github user name : ")
+url = "https://api.github.com/{}"
+
+token = "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"
+path = f"user/repos"
+
+headers = {'Authorization': 'token ' + token}
+
+response = requests.get(url.format(path) , headers=headers)
+
+data = response.json()  # Data in List data type.
+len = len(data)    
+
+print("Yours Public repos are as following :")
+for i in range(len):
+    repo = data[i]     # data have dict datatype. 
+    print(i+1, repo["name"])        # print total no. and name of repos in your Github account.
+
+
+
